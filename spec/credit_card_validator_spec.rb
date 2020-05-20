2f04457bce39cf1bf36d0d08f7b9b857188e824c
tamerl <tamer.mikhaiel@gmail.com>
2020-05-20 22:16:32 +1000

    updating the test to return correct values

--- spec/credit_card_validator_spec.rb
+++ spec/credit_card_validator_spec.rb
@@ -4,67 +4,62 @@ require './src/credit_card_validator'
 
 describe '.initialize' do
   it 'has a number' do
-    @card = CreditCardValidator.new("4408 0412 3456 7893")
     pending
-    expect(@card.num).to eq('4408 0412 3456 7893')
+    card = CreditCardValidator.new("4408 0412 3456 7893")
+    expect(card.num).to eq('4408 0412 3456 7893')
   end
 end
 
 describe '#type' do
-  # describe 'credit card validator integration' do
   context "when the credit card number has a known type" do
     it "returns the type of the credit card" do
-      card = CreditCardValidator.new("4408 0412 3456 7893")
       pending
+      card = CreditCardValidator.new("4408 0412 3456 7893")
       expect(card.type).to eq("Visa\n")
     end
 
     it "returns the type of the credit card" do
-      card = CreditCardValidator.new("5500 0000 0000 0004")
       pending
+      card = CreditCardValidator.new("5500 0000 0000 0004")
       expect(card.type).to eq("MasterCard\n")
     end
 
     it "returns the type of the credit card" do
-      card = CreditCardValidator.new("3400 0000 0000 009")
       pending
+      card = CreditCardValidator.new("3400 0000 0000 009")
       expect(card.type).to eq("AMEX\n")
     end
 
     it "returns the type of the credit card" do
-      card = CreditCardValidator.new("6011 0000 0000 0004")
       pending
+      card = CreditCardValidator.new("6011 0000 0000 0004")
       expect(card.type).to eq("Discover\n")
     end
+  end
 
-    context "when the credit card type is unknown" do
-      it "returns an error: This card type is Unknown" do
-        card = CreditCardValidator.new("7011 0000 0000 0004")
-        pending
-        expect do
-          card.type
-        end.to raise_error("This card type is Unknown")
-      end
+  context "when the credit card type is unknown" do
+    it "returns Unknown" do
+      pending
+      card = CreditCardValidator.new("7011 0000 0000 0004")
+      expect(card.type).to eq("Unknown\n")
     end
   end
 end
 
 describe '#is_valid?' do
   context "when the credit card is valid" do
-    it "returns Valid" do
-      card = CreditCardValidator.new("4111 1111 1111 1111")
+    it "returns true" do
       pending
-      expect(card.is_valid?).to eq("Valid\n")
+      card = CreditCardValidator.new("4111 1111 1111 1111")
+      expect(card.is_valid?).to eq("true\n")
     end
+  end
 
-    context "when the credit card is invalid" do
-      it "returns Invalid" do
-        card = CreditCardValidator.new("7011 0000 0000 0004")
-        pending
-        expect do
-          card.is_valid?
-        end.to raise_error("This card type is Invalid")
-      end
+  context "when the credit card is invalid" do
+    it "returns false" do
+      pending
+      card = CreditCardValidator.new("7011 0000 0000 0004")
+      expect(card.is_valid?).to eq("false\n")
     end
   end
 end
