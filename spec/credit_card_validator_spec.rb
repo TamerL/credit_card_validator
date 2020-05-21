caa662ea5c1876cf71fc7c09406b10819d45db9b
tamerl <tamer.mikhaiel@gmail.com>
2020-05-21 21:54:21 +1000

    adding new test cases and refactoring the code

--- spec/credit_card_validator_spec.rb
+++ spec/credit_card_validator_spec.rb
@@ -4,34 +4,56 @@ require './src/credit_card_validator'
 
 describe '.initialize' do
   it 'with a credit card number' do
-    # pending
     card = CreditCardValidator.new("4408 0412 3456 7893")
     expect(card.num).to eq('4408041234567893')
   end
 end
 
 describe '#type' do
+  context "when the credit card number does not follow the right format" do
+    it "raise an error" do
+      card = CreditCardValidator.new("4408a41234567893")
+      expect do
+        card.type
+      end.to raise_error("Credit card number should contain only numbers")
+    end
+  end
+
+  context "when the credit card number length is not correct" do
+    it "raise an error" do
+      card = CreditCardValidator.new("4408")
+      expect do
+        card.type
+      end.to raise_error("Credit card number length should be between 13 and 16 digits")
+    end
+  end
+
+  context "when the credit card number length is not correct" do
+    it "raise an error" do
+      card = CreditCardValidator.new("4408 0000 3333 4444 5555 6")
+      expect do
+        card.type
+      end.to raise_error("Credit card number length should be between 13 and 16 digits")
+    end
+  end
+
   context "when the credit card number has a known type" do
     it "returns Visa as a type of the credit card" do
-      # pending
       card = CreditCardValidator.new("4408 0412 3456 7893")
       expect(card.type).to eq("Visa")
     end
 
     it "returns MasterCard as a type of the credit card" do
-      # pending
       card = CreditCardValidator.new("5500 0000 0000 0004")
       expect(card.type).to eq("MasterCard")
     end
 
     it "returns AMEX as a type of the credit card" do
-      # pending
       card = CreditCardValidator.new("3400 0000 0000 009")
       expect(card.type).to eq("AMEX")
     end
 
     it "returns Discover as a type of the credit card" do
-      # pending
       card = CreditCardValidator.new("6011 0000 0000 0004")
       expect(card.type).to eq("Discover")
     end
@@ -39,7 +61,6 @@ describe '#type' do
 
   context "when the credit card type is unknown" do
     it "returns Unknown" do
-      # pending
       card = CreditCardValidator.new("7011 0000 0000 0004")
       expect(card.type).to eq("Unknown")
     end
@@ -47,15 +68,40 @@ describe '#type' do
 end
 
 describe '#is_valid?' do
+  context "when the credit card number length is not correct" do
+    it "raise an error" do
+      card = CreditCardValidator.new("4408")
+      expect do
+        card.is_valid?
+      end.to raise_error("Credit card number length should be between 13 and 16 digits")
+    end
+  end
+
+  context "when the credit card number does not follow the right format" do
+    it "raise an error" do
+      card = CreditCardValidator.new("4408a41234567893")
+      expect do
+        card.is_valid?
+      end.to raise_error("Credit card number should contain only numbers")
+    end
+  end
+
+  context "when the credit card number length is not correct" do
+    it "raise an error" do
+      card = CreditCardValidator.new("4408 0000 3333 4444 5555 6")
+      expect do
+        card.is_valid?
+      end.to raise_error("Credit card number length should be between 13 and 16 digits")
+    end
+  end
+
   context "when the credit card is valid" do
     it "returns true for case#1" do
-      # pending
       card = CreditCardValidator.new("4111 1111 1111 1111")
       expect(card.is_valid?).to eq(true)
     end
 
     it "returns true for case#2" do
-      # pending
       card = CreditCardValidator.new("4408 0412 3456 7893")
       expect(card.is_valid?).to eq(true)
     end
@@ -63,7 +109,6 @@ describe '#is_valid?' do
 
   context "when the credit card is invalid" do
     it "returns false" do
-      # pending
       card = CreditCardValidator.new("7011 0000 0000 0004")
       expect(card.is_valid?).to eq(false)
     end
--- src/credit_card_validator.rb
+++ src/credit_card_validator.rb
@@ -1,48 +1,64 @@
+require 'pry'
 class CreditCardValidator
-  attr_accessor :num
+  attr_reader :num
   def initialize(num)
-    @num = num
-    check_num(@num)
-  end
-
-  def type
-    case num
-    when -> (num) {[13,16].include?(num.length) && num[0] == "4"}
-      then
-      return "Visa"
-    when -> (num) {num.length == 16 && num[0] == "5" &&
-      (1..5).map(&:to_s).include?(num[1])}
-      then
-      return "MasterCard"
-    when -> (num) {num.length == 15 && (num[0..1] == "34" || num[0..1] == "37")}
-      then
-      return "AMEX"
-    when -> (num) {num.length == 16 && num[0..3] == "6011"}
-      then
-      return "Discover"
-    else
-      return "Unknown"
-    end
+    @num = num.gsub(' ','')
   end
 
   def is_valid?
+    validate!
     array = num.to_s.split('').map(&:to_i)
     iterator = array.length - 2
     while iterator >= 0 do
       array[iterator] *= 2
       iterator -= 2
     end
-    sum = array.join.to_s.split('').map(&:to_i).sum
-    if sum % 10 == 0
-      return true
-    else
-      return false
-    end
+    sum = array.join.split('').sum(&:to_i)
+    sum % 10 == 0
+  end
+
+  def type
+    validate!
+    is_visa || is_master || is_amex || is_discover || "Unknown"
   end
 
   private
-  def check_num(num)
-    raise "Credit card number should contain only numbers" if @num.scan(/\D/).empty?
-    @num = num.gsub(' ','')
+  #this is small check method that would be called in the type() is_valid?()
+  #if it s bigger then few lines - causing performance issues - it should
+  #be public and let the third party - outside the class - call it before init.
+  def validate!
+    raise "Credit card number should contain only numbers" if
+    !num.scan(/\D/).empty?
+    raise "Credit card number length should be between 13 and 16 digits" if
+    (num.length > 16 || num.length < 13)
   end
-end
+
+  def is_visa
+    (
+      [13,16].include?(num.length) &&
+      num[0] == "4"
+    ) ? "Visa" : nil
+  end
+
+  def is_master
+    (
+      num.length == 16 &&
+      num[0] == "5" &&
+      (1..5).map(&:to_s).include?(num[1])
+    ) ? "MasterCard" : nil
+  end
+
+      def is_amex
+        (
+          num.length == 15 &&
+          (num[0..1] == "34" || num[0..1] == "37")
+          ) ? "AMEX" : nil
+        end
+
+        def is_discover
+          (num.length == 16 &&
+            num[0..3] == "6011"
+            ) ? "Discover" : nil
+          end
+
+        end
